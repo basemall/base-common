b3d557105bb22966c492e8a6fab09900093ec65e
apollo2099 <liaoxingjian.2008@163.com>
2017-03-31 22:31:49 +0800

    新增包相互调用测试类

---
+++ src/main/java/com/ibase/mall/utils/EmailUtils.java
@@ -0,0 +1,13 @@
+package com.ibase.mall.utils;
+
+public class EmailUtils {
+
+	public static void send(){
+		System.out.println("send email success");
+	}
+	
+	
+	public static void config(){
+		System.out.println("config email success");
+	}
+}
